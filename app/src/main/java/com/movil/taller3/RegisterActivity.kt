967e28a2b37723ef9c2c84ab4eccfd18b35762db
Julian Ramos <julianc.ramos21@gmail.com>
2025-05-16 10:17:23 -0500

    feat: add user profile image using storage

--- app/src/main/java/com/movil/taller3/RegisterActivity.kt
+++ app/src/main/java/com/movil/taller3/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.movil.taller3
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
@@ -13,6 +14,7 @@ import androidx.core.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.movil.taller3.databinding.ActivityRegisterBinding
 import com.movil.taller3.models.User
 
@@ -27,6 +29,11 @@ class RegisterActivity : AppCompatActivity() {
     // coordenadas de la ubicación actual del usuario
     private var latitude: Double = 0.0
     private var longitude: Double = 0.0
+
+    //para imagen
+    private var selectedImageUri: Uri? = null
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,26 +54,129 @@ class RegisterActivity : AppCompatActivity() {
             }}
         )
 
+        //para la galería
+        galleryLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == RESULT_OK && it.data != null) {
+                selectedImageUri = it.data!!.data
+                binding.imgProfile.setImageURI(selectedImageUri)
+            }
+        }
+
+
 
     }
 
     private fun initListeners() {
         binding.btnRegister.setOnClickListener {
-            // se verifica el permiso
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-                getCurrentLocation()
-            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                Toast.makeText(this, "Se necesita permiso para obtener tu ubicación", Toast.LENGTH_SHORT).show()
-                locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-            } else {
-                locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-            }
-
-
+            validateAndGetLocation()
         }
+
+
+        binding.btnSelectImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            galleryLauncher.launch(intent)
+        }
+
     }
 
+
+    private fun registerUser() {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+        val idNumber = binding.etIdNumber.text.toString()
+
+        // crea la cuenta
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    // primero sube la imagen para obtener la url antes de registrar al usuario
+                    uploadProfileImage { imageUrl ->
+                        val uid = auth.currentUser?.uid ?: return@uploadProfileImage
+
+                        val user = User(
+                            firstName = firstName,
+                            lastName = lastName,
+                            email = email,
+                            idNumber = idNumber,
+                            latitude = latitude,
+                            longitude = longitude,
+                            imageUrl = imageUrl
+                        )
+
+                        database.child("users").child(uid).setValue(user)
+                            .addOnSuccessListener {
+                                startActivity(Intent(this, MainActivity::class.java))
+                                finish()
+                            }
+                            .addOnFailureListener {
+                                Toast.makeText(this, "Error al guardar datos", Toast.LENGTH_LONG).show()
+                            }
+                    }
+                } else {
+                    Toast.makeText(this, "Error: ${it.exception?.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+        //subir imagen al storage de firebase
+        //tiene que recibir la función por parametro porque se ejecuta una función asíncrona
+        private fun uploadProfileImage(onUrlReady: (String) -> Unit) {
+
+            val uid = auth.currentUser?.uid ?: return
+            //se sube la foto con el perfil del usuario
+            val imageRef = FirebaseStorage.getInstance().reference.child("profile_images/$uid.jpg")
+
+            //lanza operación asíncrona
+            imageRef.putFile(selectedImageUri!!)
+                .addOnSuccessListener {
+                    //se pide la url publica de la imagen subida
+                    imageRef.downloadUrl.addOnSuccessListener { uri ->
+                        //se ejecuta la función que se pasa por parametro
+                        //si acá se hiciera return en vez de ejecutar la función, entonces
+                            //se haría return antes de que llegue el valor
+                        onUrlReady(uri.toString())
+                    }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error al subir imagen", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+    private fun validateAndGetLocation() {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+        val idNumber = binding.etIdNumber.text.toString()
+
+        // Validación de campos
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() ||
+            password.isEmpty() || idNumber.isEmpty()) {
+            Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Validación de imagen
+        if (selectedImageUri == null) {
+            Toast.makeText(this, "Selecciona una imagen de perfil", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // se pide permiso para la ubicacion
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation()
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+    //obtiene la ubicación actual para guardar latitud y longitud
     private fun getCurrentLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
@@ -87,50 +197,6 @@ class RegisterActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "No se pudo obtener tu ubicación", Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun registerUser() {
-        val firstName = binding.etFirstName.text.toString()
-        val lastName = binding.etLastName.text.toString()
-        val email = binding.etEmail.text.toString()
-        val password = binding.etPassword.text.toString()
-        val idNumber = binding.etIdNumber.text.toString()
-
-        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() ||
-            password.isEmpty() || idNumber.isEmpty()) {
-            Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        //crea la cuenta
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    val uid = auth.currentUser?.uid ?: return@addOnCompleteListener
-
-                    val user = User(
-                        firstName = firstName,
-                        lastName = lastName,
-                        email = email,
-                        idNumber = idNumber,
-                        latitude = latitude,
-                        longitude = longitude
-                    )
-
-                    database.child("users").child(uid).setValue(user)
-                        .addOnSuccessListener {
-                            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
-                            startActivity(Intent(this, MainActivity::class.java))
-                            finish()
-                        }
-                        .addOnFailureListener {
-                            Toast.makeText(this, "Error al guardar datos", Toast.LENGTH_LONG).show()
-                        }
-
-                } else {
-                    Toast.makeText(this, "Error: ${it.exception?.message}", Toast.LENGTH_LONG).show()
-                }
-            }
     }
 
 }
